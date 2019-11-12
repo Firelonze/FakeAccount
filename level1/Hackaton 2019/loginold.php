306be849850dfe9c7f354f9eda657e19fa390e90
PAP0 <54799202+PAP0@users.noreply.github.com>
2019-11-12 13:44:14 +0100

    Add files via upload

--- level1/Hackaton 2019/loginold.php
+++ level1/Hackaton 2019/loginold.php
@@ -15,7 +15,7 @@
       $username = "Welcome " . test_input($_POST["username"]);
       $username = "";
       $nameError = $passError = "";
-      header("Location: successannemarie.html"); 
+      header("Location: index.html"); 
  
     }else {
       $nameError = "Dit is een foute naam";
@@ -27,7 +27,7 @@
         $username = "Welcome " . test_input($_POST["username"]);
         $username = "";
         $nameError = $passError = "";
-        header("Location: successjasmine.html");
+        echo "sup biatch";
    
       }else {
         $nameError = "Dit is een foute naam";
@@ -39,7 +39,7 @@
           $username = "Welcome " . test_input($_POST["username"]);
           $username = "";
           $nameError = $passError = "";
-          header("Location: successhenk.html"); 
+          header("Location: index.html"); 
      
         }else {
           $nameError = "Dit is een foute naam";
@@ -56,8 +56,7 @@
   <body>
     <div class="container">
     <form class="???" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="post">
-    <!-- Tips om wachtwoorden te raden: de naam van de gebruiker de namen van iemands broers en zussen. de naam van iemands huidige of favoriete (huis)dier. iemand zijn/haar geboorte datum. het adres kan voor komen als wachtwoord van een gebruiker. meestal begint het wachtwoord met een hoofdletter, let goed op wat de gebruiker heeft gezegd/gepost -->
-    <p>username:  jasmine, henk, annemarie</p>  
+    <p>username:</p>  
     <input type="text" name="username" id="username" autocomplete="off"><?php echo $nameError ?><br>
     <p>password:</p>    
     <input type="password" name="password" id="password" autocomplete="off"><?php echo $passError ?><br>
@@ -69,26 +68,8 @@
               <a href="friends.html"><i class="fa fa-fw fa-user"></i> </a>
               <a href="login.php"><i class="fa fa-fw fa-envelope"></i></a>
     </div>
-    <br>
-    <input type="submit" name="submit" value="submit">
-    <head>
-  <title>Bootstrap Example</title>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
-</head>
-<body>
-
-<div class="container">
-  <a href="#" data-toggle="popover" title="hint" data-content="Hier komt de hint">hint nodig?</a>
-</div>
-
-<script>
-$(document).ready(function(){
-    $('[data-toggle="popover"]').popover();   
-});
-</script>
-</body>
+  </body>
 </html>
+<?php
+echo $username;
+?>
