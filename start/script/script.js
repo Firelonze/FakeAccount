410c86e2d56b00dd078f02eaf6425193070e13fd
Robin van den Bor <43145241+Firelonze@users.noreply.github.com>
2019-11-13 09:20:38 +0100

    Add files via upload

--- start/script/script.js
+++ start/script/script.js
@@ -1,8 +1,14 @@
 
 
-function OpenGame()
+function OpenJasmine()
 {
-    window.open("https://www.w3schools.com");
+    window.open("../level1/inde.html");
     // sluit de pagina die je open had staan.
     // window.close("index.html");
+}
+function OpenHenk(){
+    window.open("../level3/inde.html");
+}
+function OpenAnnemarie(){
+    window.open("../level2/inde.html");
 }
